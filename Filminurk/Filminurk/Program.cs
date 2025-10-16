722e9d4da9d16d89b2cde212134a2274cee7bb30
opilane <opilane@HP-CZC3217062>
2025-10-16 10:49:51 +0300

    finished index

--- Filminurk/Filminurk/Program.cs
+++ Filminurk/Filminurk/Program.cs
@@ -1,3 +1,5 @@
+using Filminurk.ApplicationServices.Services;
+using Filminurk.Core.ServiceInterface;
 using Filminurk.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IMovieServices, MovieServices>();
 builder.Services.AddDbContext<FilminurkTARpe24Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("FilminurkConnection")));
 
 var app = builder.Build();
