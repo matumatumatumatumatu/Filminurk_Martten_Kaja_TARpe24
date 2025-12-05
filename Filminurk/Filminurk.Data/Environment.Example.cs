046c076500550aaf55036fb315c8c3ecbfa45146
opilane <opilane@HP-CZC3217062>
2025-12-05 12:20:49 +0200

    hjnjmhj

---
+++ Filminurk/Filminurk.Data/Environment.Example.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Data
+{
+    public class Environment1
+    {
+        public static string smtpaddress = "";
+        public static string gmailapppasword = "";
+        public static string gmailusername = "";
+    }
+}
