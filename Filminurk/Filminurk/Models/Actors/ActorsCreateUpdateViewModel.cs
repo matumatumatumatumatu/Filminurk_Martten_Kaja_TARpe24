94248e36b65484e97e839fa01706dbf8a8b4c4f9
opilane <opilane@HP-CZC3217062>
2026-04-09 10:47:54 +0300

    Actors - Loodud algsed andmemudelid ja Domain

---
+++ Filminurk/Filminurk/Models/Actors/ActorsCreateUpdateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Filminurk.Models.Actors
+{
+    public class ActorsCreateUpdateViewModel
+    {
+        [Key]
+        public Guid ActorID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? NickName { get; set; }
+        public List<string> MoviesActedFor { get; set; }
+        public int? PortraitID { get; set; }
+
+        public int? Age { get; set; }
+        public Nationality Nationality { get; set; }
+        public bool? IsActive { get; set; }
+        public DateTime? EntryCreatedAt { get; set; }
+        public DateTime? EntryModifiedAt { get; set; }
+    }
+}
