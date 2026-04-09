d2980cb43a299a5b9af91394cc4320199744edee
opilane <opilane@HP-CZC3217062>
2026-04-09 11:28:55 +0300

    Actors - Lisatud actors shared layout'i, tehtud uus migratsioon ja update-database

---
+++ Filminurk/Filminurk.Data/Migrations/20260409082824_init4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Filminurk.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class init4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Actors",
+                columns: table => new
+                {
+                    ActorID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NickName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MoviesActedFor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PortraitID = table.Column<int>(type: "int", nullable: true),
+                    Age = table.Column<int>(type: "int", nullable: true),
+                    Nationality = table.Column<int>(type: "int", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: true),
+                    EntryCreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EntryModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Actors", x => x.ActorID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Actors");
+        }
+    }
+}
