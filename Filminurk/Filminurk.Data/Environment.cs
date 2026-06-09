a7eba25d79bad381d93ca2bbe9301f30ee1b2007
opilane <opilane@HP-CZC3217062>
2026-06-09 12:45:20 +0300

    removed environment oopsie

--- Filminurk/Filminurk.Data/Environment.cs
+++ Filminurk/Filminurk.Data/Environment.cs
@@ -3,10 +3,10 @@ namespace Filminurk.Data;
 public class Environment
 {
 
-		public static string smtpaddress = "smtp.gmail.com";
-		public static string gmailapppasword = "zinb sqqh kokp zqzw";
-		public static string gmailusername = "kaja.martten";
-	public static string accuweatherkey = "zpka_0175117d9967416e98ebf5053aa8e8f8_a662b221";
+		public static string smtpaddress = "";
+		public static string gmailapppasword = "";
+		public static string gmailusername = "";
+	public static string accuweatherkey = "";
 
 
 }
